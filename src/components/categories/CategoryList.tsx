08b42a4444c467a90cbf7b08812fb2ef99d3c2af
Francis Lavallee <francislavallee@hotmail.com>
2022-02-12 17:00:18 -0500

    Add Categories List

---
+++ src/components/categories/CategoryList.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { Category } from '../../models/Category';
+import api from '../../services/api';
+
+const CategoryList: React.FC = () => {
+  const [categories, setCategories] = useState<Array<Category>>([]);
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = async () => {
+    api
+      .get('/categories')
+      .then((response) => setCategories(response.data))
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div>
+      <h1>CategoryList</h1>
+
+      {categories.map((category: Category) => (
+        <div key={category.id}>
+          <h2>{category.name}</h2>
+          <p>{category.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
